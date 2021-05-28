b886317bf0429f914b516c34734e8146544f2b3c
ERIK-GREFF <83592613+ERIK-GREFF@users.noreply.github.com>
2021-05-28 19:03:29 -0300

    exercicios

--- Blog-pessoal/src/main/java/org/erik/Blog/pessoal/model/Postagem.java
+++ Blog-pessoal/src/main/java/org/erik/Blog/pessoal/model/Postagem.java
@@ -1,37 +1,43 @@
 package org.erik.Blog.pessoal.model;
 
-
-
 import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity
 @Table(name = "Postagem")
 public class Postagem {
-	
+
 	@Id
-	@GeneratedValue (strategy = GenerationType.IDENTITY)
-	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
+
 	@NotNull
 	@Size(min = 5, max = 500)
 	private String texto;
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("postagem")
+	private Tema tema;
 
 	public long getId() {
 		return id;
@@ -65,6 +71,12 @@ public class Postagem {
 		this.data = data;
 	}
 
-	
-	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
